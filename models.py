cc70cd2709a18d27bc04fc2049530614bc9be3ec
Sofia Burdelake Bespalhuk <151930139+SofiaBB2@users.noreply.github.com>
2025-04-29 11:42:26 -0300

    Atualização models.py

--- models.py
+++ models.py
@@ -7,12 +7,13 @@ meu_bd = SqliteDatabase("meus_dados.db")
 class BaseModel(Model):
     class Meta:
         database = meu_bd
-
-# Representa o Tipo de Doce com seus atributos (classificacao(bolo, bombom, eclair, etc...), 
-#                                               sabor(limão, chocolate, etc..),  
-#                                               tipo de preço(por unidade ou kg),
-#                                               preço e 
-#                                               se está disponível ou não).
+"""
+    Representa o Tipo de Doce com seus atributos (classificacao(bolo, bombom, eclair, etc...), 
+                                                  sabor(limão, chocolate, etc..),  
+                                                  tipo de preço(por unidade ou kg),
+                                                  preço e 
+                                                  se está disponível ou não).
+"""
 class TipoDoce(BaseModel):
     classificacao = CharField()
     sabor = CharField()
@@ -20,14 +21,15 @@ class TipoDoce(BaseModel):
     preco = DoubleField()
     disponivel = BooleanField()
 
-# Representa o doce que stá sendo vendido, como 50g (peso) de Bolo(tipo.classificacao) de Chocolate(tipo.sabor) 
-#                                               preço por kg(False)/unidade(True) (tipo.tipoPreco) R$9,99 (tipo.preco)
-#                                               Não está Disponível.(False)/Está Disponível!(True) (tipo.disponivel).
+"""
+    Representa o doce que stá sendo vendido, como 50g (peso) de Bolo(tipo.classificacao) de Chocolate(tipo.sabor) 
+                                                  preço por kg(False)/unidade(True) (tipo.tipoPreco) R$9,99 (tipo.preco)
+                                                  Não está Disponível.(False)/Está Disponível!(True) (tipo.disponivel).
+"""
 class Doce(BaseModel):
     peso = DoubleField()
     tipo = ForeignKeyField(TipoDoce)
 
-# Criar todas as classes e depois criar BD e tabelas
+# Criar todas as classes e depois conectar BD e criar tabelas
 meu_bd.connect()
 meu_bd.create_tables([Doce, TipoDoce])
-
